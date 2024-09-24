c8b1d662fa6cbf50f292f8b0deade579ee29440b
PUNEET TIWARI <tiwaripuneet426@gmail.com>
2024-09-24 19:26:08 +0530

    Add files via upload

---
+++ Music Player 1/js/music-list.js
@@ -0,0 +1,675 @@
+let allMusic = [
+    {
+        name:"Ae Dil Hai Mushkil",
+        artist:"Arijit Singh",
+        img:"Aedilhaimushkil",
+        src:"Aedilhaimushkil"
+
+    },
+    {
+        name:"Agar Ho Tum ",
+        artist:"Arijit Singh",
+        img:"Agarhotum",
+        src:"Agarhotum"
+
+    },
+    {
+        name:"Agar Tum Saath Ho",
+        artist:"Arijit Singh",
+        img:"Agartumsaathho",
+        src:"Agartumsaathho"
+
+    },
+   
+    {
+        name:"Apna Bana Le",
+        artist:"Arijit Singh",
+        img:"Apnabanale",
+        src:"Apnabanale"
+
+    },
+    {
+        name:"Atak Gaya",
+        artist:"Arijit Singh",
+        img:"Atakgaya",
+        src:"Atakgaya"
+
+    },
+    {
+        name:"Bairiya",
+        artist:"Arijit Singh",
+        img:"Bairiya",
+        src:"Bairiya"
+
+    },
+    {
+        name:"Bandeya",
+        artist:"Arijit Singh",
+        img:"Bandeya",
+        src:"Bandeya"
+
+    },
+
+
+    {
+        name:"Bekhayali",
+        artist:"Arijit Singh",
+        img:"Bekhayali",
+        src:"Bekhayali"
+
+    },
+
+
+    {
+        name:"Bolna",
+        artist:"Arijit Singh",
+        img:"Bolna",
+        src:"Bolna"
+
+    },
+    {
+        name:"Chahun Main Ya Naa",
+        artist:"Arijit Singh",
+        img:"Chahunmainyanaa",
+        src:"Chahunmainyanaa"
+
+    },
+    {
+       
+        name:"Chal Ghar Chalen",
+        artist:"Arijit Singh",
+        img:"Chalgharchalen",
+        src:"Chalgharchalen"
+
+    },
+    {
+       
+        name:"Chaleya",
+        artist:"Arijit Singh",
+        img:"Chaleya",
+        src:"Chaleya"
+
+    },
+
+    {
+       
+        name:"Channa Mereya",
+        artist:"Arijit Singh",
+        img:"Channamereya",
+        src:"Channamereya"
+
+    },
+
+    {
+       
+        name:"Dance Ka Bhoot",
+        artist:"Arijit Singh",
+        img:"Dancekabhoot",
+        src:"Dancekabhoot"
+
+    },
+    {
+       
+        name:"Dekhha Tenu ",
+        artist:"Arijit Singh",
+        img:"Dekhhatenu",
+        src:"Dekhhatenu"
+
+    },
+    {
+        name:"Deva Deva",
+        artist:"Arijit Singh",
+        img:"Devadeva",
+        src:"Devadeva"
+
+    },
+    {
+        name:"Dil Haareya",
+        artist:"Arijit Singh",
+        img:"Dilhaareya",
+        src:"Dilhaareya"
+
+    },
+    {
+        name:"Dil Jhoom",
+        artist:"Arijit Singh",
+        img:"Diljhoom",
+        src:"Diljhoom"
+
+    },
+    {
+        name:"Dil Na Jaaneya",
+        artist:"Arijit Singh",
+        img:"Dilnajaaneya ",
+        src:"Dilnajaaneya "
+
+    },
+    {
+        name:"Ghungroo",
+        artist:"Arijit Singh",
+        img:"Ghungroo",
+        src:"Ghungroo"
+
+    },
+    {
+        name:"Haan Main Galat",
+        artist:"Arijit Singh",
+        img:"Ghungroo",
+        src:"Ghungroo"
+
+    },
+    {
+        name:"Haareya",
+        artist:"Arijit Singh",
+        img:"Haareya",
+        src:"Haareya"
+
+    },
+    {
+        name:"Hamari Adhuri Kahani",
+        artist:"Arijit Singh",
+        img:"Hamariadhurikahani",
+        src:"Hamariadhurikahani"
+
+    },
+    {
+        name:"Hawayein",
+        artist:"Arijit Singh",
+        img:"Hawayein",
+        src:"Hawayein"
+
+    },
+    {
+        name:"Heeriye",
+        artist:"Arijit Singh",
+        img:"Heeriye",
+        src:"Heeriye"
+
+    },
+    {
+        name:"Humdard",
+        artist:"Arijit Singh",
+        img:"Humdard",
+        src:"Humdard"
+
+    },
+    {
+        name:"Ik Vaari Aa",
+        artist:"Arijit Singh",
+        img:"Ikvaariaa",
+        src:"Ikvaariaa"
+
+    },
+    {
+        name:"Iktara",
+        artist:"Arijit Singh",
+        img:"Iktara",
+        src:"Iktara"
+
+    },
+    
+    {
+        name:"Intezaar",
+        artist:"Arijit Singh",
+        img:"Intezaar",
+        src:"Intezaar"
+
+    },
+    {
+        name:"Ishq Wala Love",
+        artist:"Arijit Singh",
+        img:"Intezaar",
+        src:"Intezaar"
+
+    },
+    {
+        name:"Itni Si Baat Hain ",
+        artist:"Arijit Singh",
+        img:"Itnisibaathain",
+        src:"Itnisibaathain"
+
+    },
+    {
+        name:"Jaanam",
+        artist:"Arijit Singh",
+        img:"Jaanam",
+        src:"Jaanam"
+
+    },
+    {
+        name:"Jeeja",
+        artist:"Vishal Mishra",
+        img:"Jeeja",
+        src:"Jeeja"
+
+    },
+   
+    {
+        name:"Jeene Laga Hoon",
+        artist:"Atif Alam , Shreya Ghoshal",
+        img:"Jeenelagahoon",
+        src:"Jeenelagahoon"
+
+    },
+    {
+        name:"Judaai",
+        artist:"Arijit Singh",
+        img:"Judaai",
+        src:"Judaai"
+    
+    },
+    {
+        name:"Kabhi Jo Baadal Barse",
+        artist:"Arijit Singh",
+        img:"Kabhijobaadalbarse",
+        src:"Kabhijobaadalbarse"
+    
+    },
+    {
+        name:"Kalank - Title Track",
+        artist:"Arijit Singh",
+        img:"Kalank",
+        src:"Kalank"
+    
+    },
+    {
+        name:"Kasam Se",
+        artist:"Arijit Singh",
+        img:"Kasamse",
+        src:"Kasamse"
+    
+    },
+    {
+        name:"Kesariya",
+        artist:"Arijit Singh",
+        img:"Kesariya",
+        src:"Kesariya"
+    
+    },
+    {
+        name:"Khairiyat",
+        artist:"Arijit Singh",
+        img:"Khairiyat",
+        src:"Khairiyat"
+    
+    },
+    {
+        name:"Khamoshiyan",
+        artist:"Arijit Singh",
+        img:"Khamoshiyan",
+        src:"Khamoshiyan"
+    
+    },
+    {
+        name:"Laal Ishq",
+        artist:"Arijit Singh",
+        img:"Laalishq",
+        src:"Laalishq"
+    
+    },
+    {
+        name:"Lehra Do",
+        artist:"Arijit Singh",
+        img:"Lehrado",
+        src:"Lehrado"
+    
+    },
+    {
+        name:"Lo Maan Liya",
+        artist:"Arijit Singh",
+        img:"Lomaanliya",
+        src:"Lomaanliya"
+    
+    },
+    {
+        name:"Lutt Putt Gaya (From _Dunki_)",
+        artist:"Arijit Singh",
+        img:"Luttputtgaya",
+        src:"Luttputtgaya"
+    
+    },
+    {
+        name:"Main Rang Sharbaton Ka",
+        artist:"Arijit Singh",
+        img:"Mainrangsharbatonka",
+        src:"Mainrangsharbatonka"
+    
+    },
+    {
+        name:"Mast Magan",
+        artist:"Arijit Singh",
+        img:"Mastmagan",
+        src:"Mastmagan"
+    
+    },
+    {
+        name:"Mere Yaaraa",
+        artist:"Arijit Singh",
+        img:"Mereyaaraa",
+        src:"Mereyaaraa"
+    
+    },
+    {
+        name:"Naina",
+        artist:"Arijit Singh",
+        img:"Naina",
+        src:"Naina"
+    
+    },
+    {
+        name:"O Bedardeya",
+        artist:"Arijit Singh",
+        img:"Obedardeya",
+        src:"Obedardeya"
+    
+    },
+    {
+        name:"O Mahi",
+        artist:"Arijit Singh",
+        img:"Omahi",
+        src:"Omahi"
+
+    },
+    {
+        name:"Oonchi Oonchi Deewarein",
+        artist:"Arijit Singh",
+        img:"Oonchioonchideewarein",
+        src:"Oonchioonchideewarein"
+
+    },
+    {
+        name:"Pachtaoge",
+        artist:"Arijit Singh",
+        img:"Pachtaoge",
+        src:"Pachtaoge"
+
+    },
+    {
+        name:"Pal Pal Dil Ke Paas ",
+        artist:"Arijit Singh",
+        img:"Palpaldilkepaas ",
+        src:"Palpaldilkepaas "
+
+    },
+    {
+        name:"Pal",
+        artist:"Arijit Singh",
+        img:"Pal",
+        src:"Pal"
+
+    },
+    {
+        name:"Pasoori Nu",
+        artist:"Arijit Singh",
+        img:"Pasoorinu",
+        src:"Pasoorinu"
+
+    },
+    {
+        name:"Phir Bhi Tumko Chaahunga",
+        artist:"Arijit Singh",
+        img:"Phirbhitumkochaahunga",
+        src:"Phirbhitumkochaahunga"
+
+    },
+    {
+        name:"Phir Le Aya Dil",
+        artist:"Arijit Singh",
+        img:"Phirleayadil",
+        src:"Phirleayadil"
+
+    },
+    {
+        name:"Phir Mohabbat",
+        artist:"Arijit Singh",
+        img:"Phirmohabbat",
+        src:"Phirmohabbat"
+
+    },
+    {
+        name:"Phir Aur Kya",
+        artist:"Arijit Singh",
+        img:"Phiraurkya",
+        src:"Phiraurkya"
+
+    },
+    {
+        name:"Pyaar Hota Kayi Baar Hai ",
+        artist:"Arijit Singh",
+        img:"Pyaarhotakayibaarhai",
+        src:"Pyaarhotakayibaarhai"
+
+    },
+   
+    {
+        name:"Qaafirana",
+        artist:"Arijit Singh",
+        img:"Qaafirana",
+        src:"Qaafirana"
+
+    },
+    {
+        name:"Raabta",
+        artist:"Arijit Singh",
+        img:"Raabta",
+        src:"Raabta"
+
+    },
+    {
+        name:"Rabb Warga",
+        artist:"Arijit Singh",
+        img:"Rabbwarga",
+        src:"Rabbwarga"
+
+    },
+    {
+        name:"Rait Za Rasi",
+        artist:"Arijit Singh",
+        img:"Raitzarasi",
+        src:"Raitzarasi"
+
+    },
+    {
+        name:"Rehmat",
+        artist:"Arijit Singh",
+        img:"Rehmat",
+        src:"Rehmat"
+
+    },
+    {
+        name:"Ruaan",
+        artist:"Arijit Singh",
+        img:"Ruaan",
+        src:"Ruaan"
+
+    },
+    {
+        name:"Safar",
+        artist:"Arijit Singh",
+        img:"Safar",
+        src:"Safar"
+
+    },
+   
+    {
+        name:"Sajni",
+        artist:"Arijit Singh",
+        img:"Sajni",
+        src:"Sajni"
+
+    },
+   
+    {
+        name:"Samjhawan",
+        artist:"Arijit Singh",
+        img:"Samjhawan",
+        src:"Samjhawan"
+
+    },
+    {
+        name:"Satranga",
+        artist:"Arijit Singh",
+        img:"Satranga",
+        src:"Satranga"
+
+    },
+    {
+        name:"Saware",
+        artist:"Arijit Singh",
+        img:"Saware",
+        src:"Saware"
+
+    },
+    {
+        name:"Shaukan",
+        artist:"Arijit Singh",
+        img:"Shaukan",
+        src:"Shaukan"
+
+    },
+    {
+        name:"Shayad",
+        artist:"Arijit Singh",
+        img:"Shayad",
+        src:"Shayad"
+
+    },
+    {
+        name:"Soch Na Sake",
+        artist:"Arijit Singh",
+        img:"Sochnasake",
+        src:"Sochnasake"
+
+    },
+    {
+        name:"Sooraj Dooba Hain",
+        artist:"Arijit Singh",
+        img:"Soorajdoobahain",
+        src:"Soorajdoobahain"
+
+    },
+    {
+        name:"Soulmate",
+        artist:"Arijit Singh",
+        img:"Soulmate",
+        src:"Soulmate"
+
+    },
+   
+    {
+        name:"Tera Fitoor",
+        artist:"Arijit Singh",
+        img:"Terafitoor",
+        src:"Terafitoor"
+
+    },
+   
+    {
+        name:"Tera Yaar Hoon Main",
+        artist:"Arijit Singh",
+        img:"Terayaarhoonmain",
+        src:"Terayaarhoonmain"
+
+    },
+   
+   
+    {
+        name:"Tere Hawaale",
+        artist:"Arijit Singh",
+        img:"Terehawaale",
+        src:"Terehawaale"
+
+    },
+    {
+        name:"Tere Pyaar Mein",
+        artist:"Arijit Singh",
+        img:"Terepyaarmein",
+        src:"Terepyaarmein"
+
+    },
+    {
+        name:"Tose Naina",
+        artist:"Arijit Singh",
+        img:"Tosenaina",
+        src:"Tosenaina"
+
+    },
+    {
+        name:"Tu Hain Toh ",
+        artist:"Arijit Singh",
+        img:"Tuhaintoh",
+        src:"Tuhaintoh"
+
+    },
+    
+    {
+        name:"Tu Hi Yaar Mera",
+        artist:"Arijit Singh",
+        img:"Tuhiyaarmera",
+        src:"Tuhiyaarmera"
+
+    },
+    
+    {
+        name:"Tujhe Kitna Chahne Lage",
+        artist:"Arijit Singh",
+        img:"Tujhekitnachahnelage",
+        src:"Tujhekitnachahnelage"
+
+    },
+    {
+        name:"Tum Kya Mile",
+        artist:"Arijit Singh",
+        img:"Tumkyamile",
+        src:"Tumkyamile"
+
+    },
+    
+    
+    
+    {
+        name:"Tum Hi Ho",
+        artist:"Arijit Singh",
+        img:"Tumhiho",
+        src:"Tumhiho"
+    
+    },
+    {
+        name:"Ve Kamleya ",
+        artist:"Arijit Singh",
+        img:"Vekamleya",
+        src:"Vekamleya"
+    
+    },
+   
+   
+    {
+        name:"Ve Mahi",
+        artist:"Arijit Singh",
+        img:"Vemahi",
+        src:"Vemahi"
+
+    },
+    {
+        name:"Yeh Fitoor Mera",
+        artist:"Arijit Singh",
+        img:"Yehfitoormera",
+        src:"Yehfitoormera"
+
+    },
+    {
+        name:"Zaalima",
+        artist:"Arijit Singh",
+        img:"Zaalima",
+        src:"Zaalima"
+
+    },
+   
+    
+    
+    
+   
+    
+    
+
+]
